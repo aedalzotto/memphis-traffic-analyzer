e3877ddaee7ea8630c477c3f1d9d1197f9fa1a3c
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-05-29 16:35:26 -0300

    Allow to get dataset

--- src/memphis_traffic_analyzer/dmni.py
+++ src/memphis_traffic_analyzer/dmni.py
@@ -6,26 +6,30 @@ class DMNI:
         dmni_path = "{}/debug/dmni".format(scen_path)
         dmni_logs = ["{}/{}".format(dmni_path, s) for s in listdir(dmni_path)]
 
-        self.df = pd.concat(map(pd.read_csv, dmni_logs), ignore_index=True)
+        self._df = pd.concat(map(pd.read_csv, dmni_logs), ignore_index=True)
 
-        self.df['timestamp']  = self.df['timestamp']  / 100000.0
+        self._df['timestamp']  = self._df['timestamp']  / 100000.0
 
-        self.df['cons'] = self.df['cons'].apply(str).apply(int, base=16)
-        self.df['app']  = self.df['cons'].apply(lambda x: x >> 8)
-        self.df.drop(self.df[self.df['app'] > 0xFF].index,     inplace=True)
-        self.df.drop(self.df[self.df['app'] == 0].index,       inplace=True)
+        self._df['cons'] = self._df['cons'].apply(str).apply(int, base=16)
+        self._df['app']  = self._df['cons'].apply(lambda x: x >> 8)
+        self._df.drop(self._df[self._df['app'] > 0xFF].index,     inplace=True)
+        self._df.drop(self._df[self._df['app'] == 0].index,       inplace=True)
 
-        self.df['prod'] = self.df['prod'].apply(str).apply(int, base=16)
-        self.df['cons'] = self.df['cons'] & 0xFF
-        self.df['prod'] = self.df['prod'] & 0xFF
+        self._df['prod'] = self._df['prod'].apply(str).apply(int, base=16)
+        self._df['cons'] = self._df['cons'] & 0xFF
+        self._df['prod'] = self._df['prod'] & 0xFF
 
-        self.df['total_time'] = self.df['total_time'] / 100.0
-        self.df['noc_time']   = self.df['noc_time']   / 100.0
-        self.df['dmni_time']  = self.df['total_time'] - self.df['noc_time']
+        self._df['total_time'] = self._df['total_time'] / 100.0
+        self._df['noc_time']   = self._df['noc_time']   / 100.0
+        self._df['dmni_time']  = self._df['total_time'] - self._df['noc_time']
         
-        self.df.sort_values(by='timestamp', inplace = True)
+        self._df.sort_values(by='timestamp', inplace = True)
 
-        self.df.reset_index(drop = True, inplace = True)
+        self._df.reset_index(drop = True, inplace = True)
+
+    @property
+    def df(self):
+        return self._df
 
     def __getitem__(self, key):
-        return self.df[self.df['app'] == key]
+        return self._df[self._df['app'] == key]
