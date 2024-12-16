7825aa93e6641c573d44c0b503ca6cf4b6296c45
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-12-16 17:02:34 -0300

    Add option to export to stdout in pgfplot format and generate in correct magnitude

--- src/memphis_traffic_analyzer/__init__.py
+++ src/memphis_traffic_analyzer/__init__.py
@@ -8,17 +8,19 @@ def memphis_ta():
     parser.add_argument("--start", nargs=1, type=float, metavar=('START_MS'), help="Start plotting only after START_MS (avoid warm up)", default=[0])
     parser.add_argument("--compare", nargs=2, metavar=('SCENARIO_PATH', 'APP_ID'), help="Compare to other scenario", default=[None, None])
     parser.add_argument("--save", nargs=1, metavar=("PATH"), help="Save graph to PATH", default=[None])
+    parser.add_argument("--export", action='store_true', help="Export graph points to stdout", default=False)
     args = parser.parse_args()
 
     scenario = args.SCENARIO_PATH
     app_id   = args.APP_ID
     start_ms = args.start[0]
     plt_file = args.save[0]
+    export   = args.export
 
     comp_path, comp_id = args.compare
     comp_id = [int(x) for x in comp_id] if comp_id is not None else None
 
-    ta = Graph(scenario, app_id, start_ms, comp_path, comp_id)
+    ta = Graph(scenario, app_id, start_ms, comp_path, comp_id, export)
 
     if plt_file is None:
         ta.show()
--- src/memphis_traffic_analyzer/graph.py
+++ src/memphis_traffic_analyzer/graph.py
@@ -10,7 +10,8 @@ class Graph:
             ids, 
             start_ms=None, 
             comp_path=None, 
-            comp_ids=None
+            comp_ids=None,
+            export=False
     ):
         self.figs = []
         self.axes = []
@@ -19,16 +20,16 @@ class Graph:
         for id in ids:
             traffic = dmni[id]
             if start_ms is not None:
-                traffic.drop(traffic[traffic['timestamp'] < start_ms].index, inplace=True)
+                traffic.drop(traffic[traffic['timestamp']/100000.0 < start_ms].index, inplace=True)
 
             edges = Edges(traffic)
             fig, ax = plt.subplots(len(edges))
             
             min_x = floor(
-                min([edges[edge]['timestamp'].min() for edge in edges])
+                min([edges[edge]['timestamp'].min()/100000.0 for edge in edges])
             )
             max_x = ceil(
-                max([edges[edge]['timestamp'].max() for edge in edges])
+                max([edges[edge]['timestamp'].max()/100000.0 for edge in edges])
             )
 
             for i, edge in enumerate(edges):
@@ -37,11 +38,16 @@ class Graph:
                 else:
                     ax_ref = ax
 
-                ax_ref.plot(edges[edge]['timestamp'], edges[edge]['total_time'], "o--", label="e{},{}".format(edge[0], edge[1]))
+                ax_ref.plot(edges[edge]['timestamp']/100000.0, edges[edge]['total_time']/100.0, "o--", label="e{},{}".format(edge[0], edge[1]))
                 # ax_ref.set_xlabel("Time (ms)")
                 ax_ref.set_ylabel("Latency (µs)")
                 ax_ref.legend(loc="lower right")
                 ax_ref.set_xlim([min_x, max_x])
+
+                if export:
+                    print("Edge {}".format(edge))
+                    [print("({}, {})".format(row['timestamp']/100000.0, row['total_time']/100.0)) for index, row in edges[edge].iterrows()]
+                    print("")
 
             self.figs.append(fig)
             self.axes.append(ax)
@@ -51,14 +57,14 @@ class Graph:
             for idx, comp_id in enumerate(comp_ids):
                 traffic = dmni_comp[comp_id]
                 if start_ms is not None:
-                    traffic.drop(traffic[traffic['timestamp'] < start_ms].index, inplace=True)
+                    traffic.drop(traffic[traffic['timestamp']/100000.0 < start_ms].index, inplace=True)
                 edges = Edges(traffic)
 
                 for i, edge in enumerate(edges):
                     if len(edges) > 1:
-                        self.axes[idx][i].plot(edges[edge]['timestamp'], edges[edge]['total_time'], "o--")
+                        self.axes[idx][i].plot(edges[edge]['timestamp']/100000.0, edges[edge]['total_time']/100.0, "o--")
                     else:
-                        self.axes[idx].plot(edges[edge][0], edges[edge]['total_time'], "o--")
+                        self.axes[idx].plot(edges[edge][0]/100000.0, edges[edge]['total_time']/100.0, "o--")
 
     def show(self):
         for fig in self.figs:
